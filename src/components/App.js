8048bea9b9e4583f111d1983594692ab03887e12
sagardubey47 <sagardubey47@gmail.com>
2021-03-02 17:52:23 +0530

    update components

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,97 @@
-import React,{useState, useEffect} from "react";
+// import React , {useState} from "react";
+// import "./../styles/App.css";
+// import AddTodo from "./AddTodo/AddTodo";
+// import TodoContainer from "./TodoContainer/TodoContainer";
+
+// function App() 
+// {
+// 	const [todoListValues, setTodoListValues] = useState([]);
+// 	return (
+// 	<div id="main">
+// 		<AddTodo todoListValues={todoListValues} setTodoListValues={setTodoListValues} />
+// 		<TodoContainer todoListValues={todoListValues} setTodoListValues={setTodoListValues} />
+// 	</div>
+// 	);
+// }
+
+
+// export default App;
+
+import React,{useState} from "react";
 import "./../styles/App.css";
-import Taskarea from "./Textarea" 
-import Add from "./Add"
-import Task from "./Task"
 
 function App() 
-{    
-    const [tasks , setTasks] = useState([]);
-
-	 function handleAdd(newTask, resetState) {
-		 
-		if(newTask.trim().length) {
-        setTasks( prevTasks => {return [...prevTasks, newTask]});
-		resetState();
-	  }
-	}
-
-	 function onDelete(id) {
-		
-		 setTasks((prevTasks) => {
-             return prevTasks.filter((task, index) => {
-			 return index != id;
-		 });
-		 });
-	 }
-
-	 function updateEdit(id, value) {
-		
-		if(value.trim().length) {
-		 let newtasks = tasks.slice(0, id);
-		 newtasks.push(value);
-		 tasks.slice(id+1).forEach(item => newtasks.push(item));
-		 
-		 setTasks(newtasks);
-		}
-	 }
-
-										 
-	return (
-	<div id="main">
-	   <div className="container">
-           <h2 id="heading" > todo list</h2>
-		   {   
-			   tasks.map((task, index) => {
-                  return <Task key={index} id={index}  data={task} onDelete={onDelete} updateEdit={updateEdit} />
-		       })
-		   }
-          
-		  <Taskarea handleAdd={handleAdd} />
-		  
-	   </div>
-	   
-	</div>
-	);
+{const [text, setText] = useState("");
+  const [textArray, settextArray] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [savetext, setsavetext] = useState("");
+  const [customid, setid] = useState();
+  const handleButton = () => {
+    let obj = {};
+    
+      obj.para = text;
+      let copyArray = textArray.slice();
+      copyArray.push(obj);
+      settextArray(copyArray);
+     
+    
+     setText("");
+  };
+  const handleDelete = (id) => {
+    const arr = textArray.slice();
+    const filteredArray = arr.filter((item, index) => index !==id);
+    settextArray(filteredArray);
+  };
+  const handleSave = (id) => {
+    if (savetext !== "") {
+      let copyarr = textArray.slice();
+      copyarr[id].para = savetext;
+      settextArray(copyarr);
+      setEdit(false);
+    }
+  };
+  return (
+    <div id="main" className="main">
+      <textarea
+        id="task"
+        value={text}
+        rows="1"
+        cols="20"
+        type="text"
+        onChange={(e) => setText(e.target.value)}
+      ></textarea>
+      <button id="btn" onClick={text?handleButton:null}>
+        Add todo
+      </button>
+      {textArray.map((item, index) => (
+        <div className="list" key={index}>
+          <p>{item.para}</p>
+          <span>
+            <button
+              className="edit"
+              onClick={(event) => {
+                setEdit(true);
+                setid(index);
+              }}
+            >
+              edit
+            </button>
+          </span>
+          <span>
+            <button className="delete" onClick={(event) => handleDelete(index)}>
+              delete
+            </button>
+          </span>
+        </div>
+      ))}
+      {edit && (
+        <>
+          <textarea className="editTask" onChange={(e) => setsavetext(e.target.value)}></textarea>
+          <button className="saveTask" onClick={(event) => handleSave(customid)}>save</button>
+        </>
+      )}
+    </div>
+  );
 }
 
 
